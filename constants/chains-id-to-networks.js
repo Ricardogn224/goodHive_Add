0ec4356a2a0457f9b9e411e3ce264145758702b3
Nicolas Wagner <contact@wagner-nicolas.com>
2022-04-17 16:17:24 +0200

    feat: add form to get talent

---
+++ constants/chains-id-to-networks.js
@@ -0,0 +1,13 @@
+export const CHAINS_ID_TO_NETWORKS_LIST = {
+  1: 'mainnet',
+  4: 'rinkeby',
+  5: 'goerli',
+  56: 'bsc-mainnet',
+  97: 'bsc-testnet',
+  42161: 'arbitrum_one',
+  421611: 'arbitrum_rinkeby',
+  77: 'sokol',
+  100: 'xdai',
+  137: 'polygon',
+  80001: 'mumbai',
+}
