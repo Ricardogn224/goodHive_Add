8fad7fbfcbf45199ad8c66a771849fca5ac6fa8d
Nicolas Wagner <contact@wagner-nicolas.com>
2022-04-24 10:21:21 +0200

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./pages/index.js",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
